c014433aa558a1b0129186293a51d4d59b233063
sajid-munawar <sajidmunawar666@gmail.com>
2022-08-28 00:06:06 +0500

    Add schema,data and queries

--- queries.sql
+++ queries.sql
@@ -97,23 +97,89 @@ where not species = 'digimon';
 
 -- JOIN queries one to many ralationship
 
--- What animals belong to Melody Pond?
-SELECT name,full_name FROM animals JOIN owners ON owners_id= owners.id AND owners.full_name='Melody Pond';
+-- What ANIMALS belong to Melody Pond?
+SELECT name,full_name FROM ANIMALS JOIN owners ON owners_id= owners.id AND owners.full_name='Melody Pond';
 
--- List of all animals that are pokemon (their type is Pokemon).
-SELECT animals.name AS Pokemon FROM animals JOIN species ON species_id=species.id AND name='Pokemon';
+-- List of all ANIMALS that are pokemon (their type is Pokemon).
+SELECT ANIMALS.name AS Pokemon FROM ANIMALS JOIN species ON species_id=species.id AND name='Pokemon';
 
--- List all owners and their animals, remember to include those that don't own any animal.
-SELECT name, full_name FROM animals FULL OUTER JOIN owners owner_id=owners.id;
+-- List all owners and their ANIMALS, remember to include those that don't own any animal.
+SELECT name, full_name FROM ANIMALS FULL OUTER JOIN owners owner_id=owners.id;
 
--- How many animals are there per species?
-SELECT species.name AS species FROM species JOIN animals ON species.id = species_id GROUP BY species;
+-- How many ANIMALS are there per species?
+SELECT species.name AS species FROM species JOIN ANIMALS ON species.id = species_id GROUP BY species;
 
 -- List all Digimon owned by Jennifer Orwell.
-SELECT owners.full_name AS owners, animals.name AS animals FROM owners INNER JOIN species ON owners.full_name= 'Jenifer Orwell' AND species.name =' Digimon'INNER  JOIN animals ON owners.id= owners_id AND species.id= species_id;
+SELECT owners.full_name AS owners, ANIMALS.name AS ANIMALS FROM owners INNER JOIN species ON owners.full_name= 'Jenifer Orwell' AND species.name =' Digimon'INNER  JOIN ANIMALS ON owners.id= owners_id AND species.id= species_id;
 
--- List all animals owned by Dean Winchester that haven't tried to escape.
-SELECT owners.full_name as Owners,animals.name as Animals FROM owners JOIN  animals ON   Owners.id = owner_id AND owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+-- List all ANIMALS owned by Dean Winchester that haven't tried to escape.
+SELECT owners.full_name as Owners,ANIMALS.name as ANIMALS FROM owners JOIN  ANIMALS ON   Owners.id = owner_id AND owners.full_name = 'Dean Winchester' AND ANIMALS.escape_attempts = 0;
 
--- Who owns the most animals?
-SELECT owners.full_name AS Owners,count(*) FROM owners JOIN  animals ON  owners.id = owner_id GROUP BY Owners ORDER BY count DESC LIMIT 1; 
+-- Who owns the most ANIMALS?
+SELECT owners.full_name AS Owners,count(*) FROM owners JOIN  ANIMALS ON  owners.id = owner_id GROUP BY Owners ORDER BY count DESC LIMIT 1; 
+
+SELECT owners.full_name AS Owners,count(*) FROM owners JOIN  ANIMALS ON  owners.id = owner_id GROUP BY Owners ORDER BY count DESC LIMIT 1;
+
+-- Who was the last animal seen by William Tatcher?
+SELECT ANIMALS.name 
+  FROM ANIMALS
+  JOIN  visits ON  ANIMALS.id = ANIMALS_id 
+  JOIN vets ON vets_id = vets.id WHERE vets.name = 'William Tatcher'
+  ORDER BY date_of_visit DESC LIMIT 1;
+
+  -- How many different ANIMALS did Stephanie Mendez see?
+  SELECT COUNT(DISTINCT ANIMALS.name)
+  FROM ANIMALS
+  JOIN  visits ON  ANIMALS.id = ANIMALS_id 
+  JOIN vets ON vets_id = vets.id WHERE vets.name = 'Stephanie Mendez';
+
+-- List all vets and their specialties, including vets with no specialties.
+SELECT vets.name AS vet,species.name AS specialization
+  FROM vets
+  JOIN  specializations ON  vets.id = vets_id 
+  JOIN species ON species_id = species.id;
+
+  -- List all ANIMALS that visited Stephanie Mendez between April 1st and August 30th, 2020.
+SELECT ANIMALS.name
+  FROM ANIMALS
+  JOIN  visits ON  ANIMALS.id = ANIMALS_id 
+  JOIN vets ON vets_id = vets.id WHERE vets.name = 'Stephanie Mendez' 
+  AND date_of_visit BETWEEN '2020-04-01' AND '2020-08-30';
+
+  -- What animal has the most visits to vets?
+SELECT ANIMALS.name, count(*)
+  FROM ANIMALS
+  JOIN  visits ON  ANIMALS.id = ANIMALS_id 
+  JOIN vets ON vets_id = vets.id 
+  GROUP BY ANIMALS.name ORDER BY count DESC LIMIT 1;
+
+  -- Who was Maisy Smith's first visit?
+SELECT ANIMALS.name,date_of_visit
+  FROM ANIMALS
+  JOIN  visits ON  ANIMALS.id = ANIMALS_id 
+  JOIN vets ON vets_id = vets.id WHERE vets.name = 'Maisy Smith' 
+  ORDER BY date_of_visit LIMIT 1;
+
+  -- Details for most recent visit: animal information, vet information, and date of visit.
+SELECT ANIMALS.name,date_of_birth,escape_attempts,neutered,weight_kg,species.name as specie,
+      vets.name as vet_name,vets.age as vet_age,date_of_graduation ,date_of_visit
+  FROM species 
+  JOIN ANIMALS ON ANIMALS.species_id = species.id
+  JOIN  visits ON  ANIMALS.id = ANIMALS_id 
+  JOIN vets ON visits.vets_id = vets.id
+  ORDER BY date_of_visit DESC LIMIT 1;
+
+  -- How many visits were with a vet that did not specialize in that animal's species?
+  SELECT vets.name,count(*) 
+    FROM visits
+    JOIN  vets ON vets.id = visits.vets_id  
+    LEFT JOIN specializations ON vets.id = specializations.vets_id WHERE specializations.vets_id IS NULL
+    GROUP BY vets.name;
+
+-- What specialty should Maisy Smith consider getting? Look for the species she gets the most.
+  SELECT species.name AS specialization,count(*) AS number_of_visits
+    FROM species
+    JOIN  ANIMALS ON species.id = ANIMALS.species_id
+    JOIN visits ON ANIMALS.id  = visits.ANIMALS_id
+    JOIN vets ON visits.vets_id = vets.id  WHERE vets.name = 'Maisy Smith'
+    GROUP BY species.name ORDER BY number_of_visits DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -23,3 +23,30 @@ CREATE TABLE species(
     id SERIAL PRIMARY KEY NOT NULL,
     name varchar(100) NOT NULL
 );
+
+CREATE TABLE vets ( 
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id) 
+);
+
+-- Create a join table named specializations
+CREATE TABLE specializations (
+    vets_id INT,
+    species_id INT,
+    PRIMARY KEY(vets_id, species_id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id),
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id)
+);
+
+
+-- Create a join table named visits
+CREATE TABLE visits (
+    animals_id INT,
+    vets_id INT,
+    date_of_visit DATE,
+    PRIMARY KEY(animals_id,vets_id,date_of_visit),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
