c4cd6fa8e662f29443a68312203910e2d0cf0f1f
sajid-munawar <sajidmunawar666@gmail.com>
2022-08-27 12:46:39 +0500

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,13 +1,25 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE ANIMALS (
- ID INT,
- NAME VARCHAR,
+ ID SERIAL PRIMARY KEY NOT NULL,
+ NAME VARCHAR(100) NOT NULL,
  DATE_OF_BIRTH DATE,
  ESCAPE_ATTEMPTS INT,
  NEUTERED BOOLEAN,
- WEIGHT_KG DECIMAL
- );
+ WEIGHT_KG DECIMAL,
+ species_id,
+ owners_id,
+ FOREIGN KEY (owners_id) REFERENCES owners(id),
+ FOREIGN KEY (species_id) REFERENCES species(id)
+);
 
- ALTER TABLE ANIMALS
-ADD SPECIES VARCHAR;
+CREATE TABLE owners (
+    id int SERIAL PRIMARY KEY NOT NULL,
+    full_name varchar(100) NOT NULL,
+    age int NOT NULL
+);
+
+CREATE TABLE species(
+    id int SERIAL PRIMARY KEY NOT NULL,
+    name varchar(100) NOT NULL
+)
